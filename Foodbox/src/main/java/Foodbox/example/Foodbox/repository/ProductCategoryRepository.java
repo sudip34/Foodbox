84d68dfacfa624f13b5a5250dbd0efa33abf2599
sudip34 <80578123+sudip34@users.noreply.github.com>
2023-02-20 11:12:45 +0100

    Update ProductCategoryRepository.java
    removed comments

--- Foodbox/src/main/java/Foodbox/example/Foodbox/repository/ProductCategoryRepository.java
+++ Foodbox/src/main/java/Foodbox/example/Foodbox/repository/ProductCategoryRepository.java
@@ -7,10 +7,9 @@ import Foodbox.example.Foodbox.entity.ProductCategory;
 
 	
 
-//because we don't want to take the defaults so we,r adding  
-// @RepositoryRestResource(collectionResource, path) without s at the end
 
-//@RepositoryRestResource(collectionResourceRel="productCategory", path="product-category")
+
+
 @Repository
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer> {
 
